867b1b15d0de9db71c528253f8b9a23f4ebec7f4
ricko-v <veriyantoricko302@gmail.com>
2022-06-07 09:51:18 +0700

    Rollback Purgecss

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,7 +38,6 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    'nuxt-purgecss'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
