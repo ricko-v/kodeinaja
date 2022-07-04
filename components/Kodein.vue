729616ef5515a7ff723996b26b85c37a124f0c53
ricko-v <veriyantoricko302@gmail.com>
2022-07-04 19:55:30 +0700

    change text btn

---
+++ components/Kodein.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <a :href='"/ngoding/"+src' target='_blank'>
+            <button class='btn btn-success shadow-none'>Kodein Aja</button>
+        </a>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        src: {
+            type: String
+        }
+    }
+}
+</script>
