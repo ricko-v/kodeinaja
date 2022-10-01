95e0c66cd4e285f219716fb4c7a8e5930d677558
ricko-v <ricko-v@users.noreply.github.com>
2022-10-01 08:53:53 +0000

    deploy: 12594cd61d55214a3a6e12ffaf7b3eff7b099c39

--- _nuxt/static/1664613370/tag/md/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1664613370",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftag\u002Fmd",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"81bde4f7"}}}}(null));
