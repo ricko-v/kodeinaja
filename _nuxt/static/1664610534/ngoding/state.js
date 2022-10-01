25c468763bdd2fb667649f0d8e651202eba59486
ricko-v <ricko-v@users.noreply.github.com>
2022-10-01 08:36:19 +0000

    deploy: 94364f0cf9cf6c0eb5ca3789a9222574d8df03f4

--- _nuxt/static/1664610534/ngoding/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fkodeinaja\u002F_nuxt\u002Fstatic\u002F1664610534",layout:"default",error:a,serverRendered:true,routePath:"\u002Fngoding",config:{_app:{basePath:"\u002Fkodeinaja\u002F",assetsPath:"\u002Fkodeinaja\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0c706a11"}}}}(null));
--- _nuxt/static/1664610534/user/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__={staticAssetsBase:"\u002Fkodeinaja\u002F_nuxt\u002Fstatic\u002F1664610534",layout:"default",error:{statusCode:404,message:"404 Not Found"},serverRendered:false,routePath:"\u002Fuser",config:{_app:{basePath:"\u002Fkodeinaja\u002F",assetsPath:"\u002Fkodeinaja\u002F_nuxt\u002F",cdnURL:null},content:{dbHash:"0c706a11"}}};
