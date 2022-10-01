803c12a45967a8c150ad20d54924aeda1f65aab7
ricko-v <ricko-v@users.noreply.github.com>
2022-10-01 07:49:03 +0000

    deploy: ca59993cafab8bed12b5ea9cfd3c97924721d43e

--- _nuxt/static/1664609982/p/menginstall-codeigniter-4/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fkodeinaja\u002F_nuxt\u002Fstatic\u002F1664609982",layout:"default",error:a,serverRendered:true,routePath:"\u002Fp\u002Fmenginstall-codeigniter-4",config:{_app:{basePath:"\u002Fkodeinaja\u002F",assetsPath:"\u002Fkodeinaja\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"2888093b"}}}}(null));
---
+++ _nuxt/static/1664610534/p/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002Fkodeinaja\u002F_nuxt\u002Fstatic\u002F1664610534",layout:"default",error:{statusCode:404,message:"404 Not Found"},serverRendered:false,routePath:"\u002Fp",config:{_app:{basePath:"\u002Fkodeinaja\u002F",assetsPath:"\u002Fkodeinaja\u002F_nuxt\u002F",cdnURL:null},content:{dbHash:"0c706a11"}}};
