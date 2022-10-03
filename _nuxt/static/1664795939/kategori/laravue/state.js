9e60bd42f95bb2b81ebf0128918fa66faff222cb
ricko-v <ricko-v@users.noreply.github.com>
2022-10-03 11:19:11 +0000

    deploy: 5de434d8e8a5f74539b2166d118b817e2993c09c

---
+++ _nuxt/static/1664795939/kategori/laravue/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1664795939",layout:"default",error:a,serverRendered:true,routePath:"\u002Fkategori\u002Flaravue",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"dfc53f4a"}}}}(null));
---
+++ _nuxt/static/1664795939/p/tutorial-berkontribusi-di-kodeinaja/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1664795939",layout:"default",error:a,serverRendered:true,routePath:"\u002Fp\u002Ftutorial-berkontribusi-di-kodeinaja",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"dfc53f4a"}}}}(null));
