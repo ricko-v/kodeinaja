0a72b00b5e4461333861feb6176d711579d05522
ricko-v <ricko-v@users.noreply.github.com>
2022-10-11 02:36:47 +0000

    deploy: 2d7adf85fa38ee1102d4c3ed6bc5c2ee50bcad04

---
+++ _nuxt/static/1665455795/p/ci4-routing-query-dan-parameter/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1665455795",layout:"default",error:a,serverRendered:true,routePath:"\u002Fp\u002Fci4-routing-query-dan-parameter",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"719a46e2"}}}}(null));
---
+++ _nuxt/static/1665455795/showcase/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1665455795",layout:"default",error:a,serverRendered:true,routePath:"\u002Fshowcase",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"719a46e2"}}}}(null));
