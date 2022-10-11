8dfa64f429c310b81fcf20a9f6f5d8e4878d7502
ricko-v <ricko-v@users.noreply.github.com>
2022-10-11 02:07:15 +0000

    deploy: b76b7fb5d97622953f6ca8b5193076750bf132cf

---
+++ _nuxt/static/1665454022/tag/http/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1665454022",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftag\u002Fhttp",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f2be4d06"}}}}(null));
